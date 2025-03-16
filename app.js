ce3df61aa8849a6c3ff90e3c34b6c59a1c7669fe
Eliana Acevedo <elianapinilla2011@gmail.com>
2025-03-15 22:21:56 -0500

    Actualizacion del proyecto de amigo secreto

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let input = document.getElementById("amigo");
+    let nombre = input.ariaValueMax.trim();
+
+    if(nombre === ""){
+        alert("Por favor, inserte un nombre");
+        return;
+    }
+
+    amigos.push(nombre);
+
+    actualizarlista();
+    input.value = "";
+
+    console.log(amigos);
+}
