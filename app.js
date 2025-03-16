b4322fa6e1c1df761f5a4e1c5fcf712d992ed21a
Eliana Acevedo <elianapinilla2011@gmail.com>
2025-03-16 09:39:59 -0500

    Actualizacion de la funcion sortear amigo

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let amigos = [];
 
 function agregarAmigo() {
     let input = document.getElementById("amigo");
-    let nombre = input.ariaValueMax.trim();
+    let nombre = input.value.trim();
 
     if(nombre === ""){
         alert("Por favor, inserte un nombre");
@@ -26,5 +26,17 @@ function actualizarlista(){
         li.textContent = amigos[i];
         lista.appendChild(li);
     }
+}
 
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("La lista está vacía. Agrega al menos un amigo antes de sortear.");
+        return;
+    }
+
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length); // Genera un índice aleatorio
+    let amigoSorteado = amigos[indiceAleatorio]; // Obtiene el nombre del amigo
+
+    // Mostrar el resultado en el HTML
+    document.getElementById("resultado").innerHTML = `<li>${amigoSorteado}</li>`;
 }
